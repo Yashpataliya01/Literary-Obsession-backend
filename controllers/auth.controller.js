6fdd63efc11d608717a3960f534becb2490f056b
yashpataliya01 <yashpataliya01@gmail.com>
2024-08-20 22:22:32 +0530

    first commit

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -46,10 +46,10 @@ export const signin = async(req, res) => {
         return res.status(400).json({message:"Check Password Again"})
       }
       if(result) {
-        let token = jwt.sign({email: email},"hellow");
+        let token = jwt.sign({email: email},process.env.JWT_SECRET);
         res.cookie("token", token, {
           httpOnly: true,
-          secure: true,
+          secure: process.env.NODE_ENV === "production",
           sameSite: "None"
         });
         return res.status(200).json({ message: user });
--- index.js
+++ index.js
@@ -12,8 +12,8 @@ const app = express();
 mongoose.connect("mongodb+srv://yashpataliya01:yashdeep@cluster0.6e3mz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 app.use(cors({
-  origin: ["https://literary-obsession-frontend.vercel.app"],
-  credentials: true, 
+  origin: "https://literary-obsession-frontend.vercel.app",
+  credentials: true,
 }));
 
 app.use(express.json());
