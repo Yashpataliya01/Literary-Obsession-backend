5b95c11bed0ff12114da2c111a7641d65c24b77d
yashpataliya01 <yashpataliya01@gmail.com>
2024-08-20 14:10:10 +0530

    first commit

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 mongoose.connect(process.env.mongoDb_server)
 
-app.use(cors());
+app.use(cors("https://literary-obsession-frontend.vercel.app/"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
