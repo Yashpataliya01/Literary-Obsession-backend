c658ae38735ab79406608bf52ad8746ad0f5c4a4
yashpataliya01 <yashpataliya01@gmail.com>
2024-08-22 07:02:40 +0530

    first commit

--- utils/Verifyuser.js
+++ utils/Verifyuser.js
@@ -3,12 +3,12 @@ import * as dotenv from "dotenv";
 dotenv.config()
 
 export const verifyToken = (req, res, next) => {
+  const token = req.body.token || req.headers.authorization?.split(' ')[1];
   console.log("req", req)
-  const token = req.cookies.token;
   if (!token) {
     return res.status(401).send('Token not found');
   }
-  jwt.verify(token, "jazzyyyy", (err, user) => {
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
     if (err) {
       return res.status(401).send('Unauthorized');
     }
